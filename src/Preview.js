87557ec3131cad5744a0d409a69e44f3bf09ef25
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2018-07-03 00:14:57 +0530

    Change markdown parser

--- src/Preview.js
+++ src/Preview.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactMarkdown from 'react-markdown';
 import './Editor.css';
 import { Card, CardHeader } from 'reactstrap';
+import MarkedownIt from 'markdown-it';
 
 const paneStyle = {
   borderRadius: '0px',
@@ -10,15 +11,23 @@ const paneStyle = {
 const headerStyle = {
   textAlign: 'center'
 };
+const md = MarkedownIt();
 
 class Preview extends Component {
+  createMarkup() {
+    return { __html: this.props.markdown };
+  }
+
   render() {
     return (
       <Card style={paneStyle}>
         <CardHeader tag="h4" style={headerStyle}>
           Preview
         </CardHeader>
-        <ReactMarkdown source={this.props.markdown} />
+        <div
+          style={{ overflow: 'auto' }}
+          dangerouslySetInnerHTML={{ __html: md.render(this.props.markdown) }}
+        />
       </Card>
     );
   }
