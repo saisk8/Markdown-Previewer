14f97e1e896fe3ca3b61a920b026f8e100236d46
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2018-07-02 20:38:18 +0530

    Adjust CSS

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div>
         <div className="Heading">
           <header className="App-header">
-            <h1 className="App-title">A Markdown Previewer</h1>
+            <h2>A Markdown Previewer</h2>
             <p className="App-intro">
               A simple React App that enables you to write and preview your
               Markdown
